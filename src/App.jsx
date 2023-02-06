23b9f5a150e43829406f19f7996cfbe4df93cb83
Cesare10-dev <cesare1087@gmail.com>
2023-02-06 13:09:22 +0100

    changed colors

--- src/App.jsx
+++ src/App.jsx
@@ -10,32 +10,32 @@ function App() {
     <div className="h-screen w-full flex flex-col justify-center items-center bg-[#1D9BF0] text-gray-500">
       <div className="flex items-center h-40 w-[600px] justify-between bg-black rounded-lg p-4">
         <div className="flex items-center space-x-2 cursor-pointer hover:text-blue-500 group">
-          <IoStatsChart
-            className="icons group-hover:bg-blue-900"
-            title="View"
-          />
-          <p>143</p>
-        </div>
-        <div className="flex items-center space-x-2 cursor-pointer hover:text-blue-500 group">
           <BiMessageRounded
             className="icons group-hover:bg-blue-900"
             title="Reply"
           />
           <p>4</p>
         </div>
-        <div className="flex items-center space-x-2 cursor-pointer hover:text-red-500 group">
+        <div className="flex items-center space-x-2 cursor-pointer hover:text-green-500 group">
           <HiOutlineArrowsUpDown
-            className="icons group-hover:bg-red-900"
+            className="icons group-hover:bg-green-900"
             title="Retweet"
           />
           <p>4</p>
         </div>
-        <div className="flex items-center space-x-2 cursor-pointer hover:text-green-500 group">
+        <div className="flex items-center space-x-2 cursor-pointer hover:text-red-500 group">
           <AiOutlineHeart
-            className="icons group-hover:bg-green-900"
+            className="icons group-hover:bg-red-900"
             title="Like"
           />
           <p>4</p>
+        </div>
+        <div className="flex items-center space-x-2 cursor-pointer hover:text-blue-500 group">
+          <IoStatsChart
+            className="icons group-hover:bg-blue-900"
+            title="View"
+          />
+          <p>143</p>
         </div>
         <div className="cursor-pointer hover:text-blue-500 group">
           <FiShare className="icons group-hover:bg-blue-900" title="Share" />
